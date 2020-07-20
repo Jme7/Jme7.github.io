572e1870ad468a6b5470f7e5b2e95d51cee3e91a
Jme7 <68490028+Jme7@users.noreply.github.com>
2020-07-20 10:20:20 -0600

    Update useful functions.js

--- javascript/useful functions.js
+++ javascript/useful functions.js
@@ -1,11 +1,11 @@
 int getById function(id) {
-  document.getElementById(id);
+  return document.getElementById(id);
 };
 int getByClass function(class) {
-  document.getElementByClass(class);
+  return document.getElementByClass(class);
 };
 int getByTag function(tag) {
-  document.getElementByTagName(tag);
+  return document.getElementByTagName(tag);
 };
 int valueOf function(selector) {
   return selector.value
